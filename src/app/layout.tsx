45c4a64a5df30a901df9f5c96e2a672989b72aba
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-25 13:07:51 +0700

    feat: add navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar/Navbar";
 import { Inter } from "next/font/google";
 import { FC, ReactNode } from "react";
 import "./globals.css";
@@ -17,7 +18,12 @@ interface RootLayoutProps {
 const RootLayout: FC<RootLayoutProps> = (props: RootLayoutProps) => {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{props.children}</body>
+			<body className={inter.className}>
+				<div>
+					<Navbar />
+					{props.children}
+				</div>
+			</body>
 		</html>
 	);
 };
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface NavbarProps {}
+
+const Navbar: FC<NavbarProps> = (props: NavbarProps) => {
+	return <div className="absolute top-0 bg-lime h-12 w-full"></div>;
+};
+
+export default Navbar;
