b2d87d735bc095d992f33a503098a92d171fb27e
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-25 13:31:14 +0700

    feat: add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { FC } from "react";
+
+interface DashboardProps {}
+
+const Dashboard: FC<DashboardProps> = (props: DashboardProps) => {
+	return <div>Dashboard</div>;
+};
+
+export default Dashboard;
