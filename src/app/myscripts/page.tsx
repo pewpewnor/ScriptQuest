a7ce9a020c0ccb9b90938106804090ca077a4678
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-25 21:12:45 +0700

    feat: add feature to create new script
    History-Rewrite-Original-Commit: 7f6a9695e4420afa7ac2d8598a49e1b6ef8fb8a8

--- src/app/myscripts/page.tsx
+++ src/app/myscripts/page.tsx
@@ -3,6 +3,8 @@ import Editor from "@/components/editor/Editor";
 import Navbar from "@/components/navbar/Navbar";
 import { ChangeEvent, FC, useState } from "react";
 
+const MAX_TITLE_LENGTH = 50;
+
 interface ScriptData {
 	title: string;
 	code: string;
@@ -17,14 +19,32 @@ const MyScripts: FC<MyScriptsProps> = (props: MyScriptsProps) => {
 			code: "",
 		},
 	]);
-	const [input, setInput] = useState<ScriptData>({ title: "", code: "" });
+	const [inputData, setInputData] = useState<ScriptData>({
+		title: "",
+		code: "",
+	});
 
-	function addScript(title: string) {
-		setScripts((prev) => [...prev, { title: title, code: "" }]);
+	function addScript() {
+		if (inputData.title.length > MAX_TITLE_LENGTH) {
+			alert(
+				`Script title cannot be more than ${MAX_TITLE_LENGTH} characters`
+			);
+			return;
+		}
+		setScripts((prev) => [...prev, { title: inputData.title, code: "" }]);
 	}
 
 	function handleChange(event: ChangeEvent<HTMLInputElement>) {
-		setInput((prev) => ({
+		if (
+			event.target.name === "title" &&
+			event.target.value.length > MAX_TITLE_LENGTH
+		) {
+			alert(
+				`Script title cannot be more than ${MAX_TITLE_LENGTH} characters`
+			);
+			return;
+		}
+		setInputData((prev) => ({
 			...prev,
 			[event.target.name]: event.target.value,
 		}));
@@ -33,7 +53,7 @@ const MyScripts: FC<MyScriptsProps> = (props: MyScriptsProps) => {
 	return (
 		<>
 			<Navbar leaveWarning={true} />
-			<div className="flex w-full flex-col justify-around gap-16 px-10 pb-14 pt-44 sm:px-24 lg:px-40">
+			<div className="flex w-full flex-col justify-around gap-16 px-10 pb-14 pt-44 sm:px-24 lg:px-56">
 				<h1 className="mb-4 w-full text-center font-press text-4xl leading-normal text-electric underline sm:text-5xl md:text-6xl">
 					MY SCRIPTS
 				</h1>
@@ -45,11 +65,14 @@ const MyScripts: FC<MyScriptsProps> = (props: MyScriptsProps) => {
 						placeholder="script title..."
 						className="w-full rounded-lg px-4 py-2 outline-none"
 					/>
-					<button className="w-40 rounded-lg bg-lime p-2 font-vt text-2xl text-dark hover:bg-electric sm:w-72">
+					<button
+						className="w-40 rounded-lg bg-lime p-2 font-vt text-2xl text-dark hover:bg-electric sm:w-72"
+						onClick={addScript}
+					>
 						Create Script
 					</button>
 				</div>
-				<div>
+				<div className="flex flex-col gap-14">
 					{scripts.map((script) => (
 						<div
 							key={script.title}
