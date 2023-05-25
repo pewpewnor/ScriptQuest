a5a0fc8bbc83bf0e3be53d5f3720557833662fae
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-25 13:25:30 +0700

    style: edit responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ const WelcomePage: FC<WelcomePageProps> = (props: WelcomePageProps) => {
 	return (
 		<div className="min-h-screen">
 			{/* Hero */}
-			<div className="paper-grid flex h-screen flex-col items-center justify-center gap-16 bg-dark px-8 pt-8 md:px-16 mid:flex-row mid:gap-0">
+			<div className="paper-grid flex h-screen flex-col items-center justify-center gap-24 bg-dark px-8 pt-8 md:px-16 mid:flex-row mid:gap-0">
 				<div className="md:w-6/7 flex w-full flex-col gap-10 px-0 sm:px-12 mid:w-full">
 					<p className="w-full text-center text-xl font-bold text-electric underline sm:text-left md:text-2xl">
 						LET&apos;S LEARN HOW TO
 					</p>
 					<div className="flex flex-col gap-8 font-press text-3xl sm:text-5xl mid:text-6xl">
 						<div className="flex flex-col items-center justify-center gap-8 sm:flex-row sm:justify-normal">
-							<button className="min-w-min -skew-y-2 rounded-md bg-lime p-6 text-dark hover:bg-electric hover:text-clay">
+							<button className="min-w-min -skew-y-2 rounded-md bg-lime p-6 text-dark hover:bg-electric hover:text-clay active:bg-electric">
 								SCRIPT
 							</button>
 							<h1 className="text-clay">A</h1>
