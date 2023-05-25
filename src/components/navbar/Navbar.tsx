55f6cbb0be1efc278be497f15f29b4727ead1587
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-25 14:05:59 +0700

    feat: add condition to show leave warning

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
 import { FC } from "react";
 
-interface NavbarProps {}
+interface NavbarProps {
+	leaveWarning: boolean;
+}
 
 const Navbar: FC<NavbarProps> = (props: NavbarProps) => {
+	if (props.leaveWarning) {
+		return (
+			<div className="absolute top-0 flex h-12 w-full items-center justify-around bg-black font-vt text-2xl text-red-400">
+				<p>WARNING: LEAVING THIS PAGE WILL RESULT IN LOSS OF DATA</p>
+			</div>
+		);
+	}
+
 	return (
 		<div className="absolute top-0 flex h-12 w-full items-center justify-around bg-black font-vt text-2xl text-clay">
 			<Link
@@ -14,6 +24,8 @@ const Navbar: FC<NavbarProps> = (props: NavbarProps) => {
 			</Link>
 			<Link
 				href="/dashboard"
+				rel="noopener noreferrer"
+				target="_blank"
 				className="hover:text-white hover:underline active:text-white active:underline"
 			>
 				DASHBOARD
