1a86197efd0aded0bda86d868b46b046cfe9189d
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-25 13:25:39 +0700

    feat: add links to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,31 @@
+import Link from "next/link";
 import { FC } from "react";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = (props: NavbarProps) => {
-	return <div className="absolute top-0 h-12 w-full bg-lime"></div>;
+	return (
+		<div className="absolute top-0 flex h-12 w-full items-center justify-around bg-black font-vt text-2xl text-clay">
+			<Link
+				href="/home"
+				className="hover:text-white hover:underline active:text-white active:underline"
+			>
+				HOME
+			</Link>
+			<Link
+				href="/dasboard"
+				className="hover:text-white hover:underline active:text-white active:underline"
+			>
+				DASHBOARD
+			</Link>
+			<Link
+				href="/pupose"
+				className="hover:text-white hover:underline active:text-white active:underline"
+			>
+				PURPOSE
+			</Link>
+		</div>
+	);
 };
 
 export default Navbar;
