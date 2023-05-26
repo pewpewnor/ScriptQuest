ddc03eeea436d52e9f388a523932f1997e75a09f
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-26 13:13:20 +0700

    feat: add type and default value for scriptdata
    History-Rewrite-Original-Commit: f88ce666e0e7bd195f84cf350318a0e849e4d73b

---
+++ src/types/script-type.ts
@@ -0,0 +1,12 @@
+interface ScriptData {
+	title: string;
+	code: string;
+}
+
+const DEFAULT_SCRIPTDATA_VALUE = {
+	title: "",
+	code: "",
+};
+
+export type { ScriptData };
+export { DEFAULT_SCRIPTDATA_VALUE };
