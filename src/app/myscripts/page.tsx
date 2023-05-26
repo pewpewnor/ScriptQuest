be4568baa60fc9db8cf346df9ac0f7e72254792f
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-26 12:43:41 +0700

    feat: add script title validations

--- src/app/myscripts/page.tsx
+++ src/app/myscripts/page.tsx
@@ -13,12 +13,7 @@ interface ScriptData {
 interface MyScriptsProps {}
 
 const MyScripts: FC<MyScriptsProps> = (props: MyScriptsProps) => {
-	const [scripts, setScripts] = useState<ScriptData[]>([
-		{
-			title: "script1",
-			code: "",
-		},
-	]);
+	const [scripts, setScripts] = useState<ScriptData[]>([]);
 	const [inputData, setInputData] = useState<ScriptData>({
 		title: "",
 		code: "",
@@ -30,8 +25,19 @@ const MyScripts: FC<MyScriptsProps> = (props: MyScriptsProps) => {
 				`Script title cannot be more than ${MAX_TITLE_LENGTH} characters`
 			);
 			return;
+		} else if (inputData.title.length === 0) {
+			alert("Script title cannot be empty");
+			return;
+		} else if (scripts.some((script) => script.title === inputData.title)) {
+			alert("Script title must be unique");
+			return;
 		}
-		setScripts((prev) => [...prev, { title: inputData.title, code: "" }]);
+
+		setScripts((prev) => [{ title: inputData.title, code: "" }, ...prev]);
+		setInputData({
+			title: "",
+			code: "",
+		});
 	}
 
 	function handleChange(event: ChangeEvent<HTMLInputElement>) {
@@ -44,6 +50,7 @@ const MyScripts: FC<MyScriptsProps> = (props: MyScriptsProps) => {
 			);
 			return;
 		}
+
 		setInputData((prev) => ({
 			...prev,
 			[event.target.name]: event.target.value,
@@ -53,23 +60,23 @@ const MyScripts: FC<MyScriptsProps> = (props: MyScriptsProps) => {
 	return (
 		<>
 			<Navbar leaveWarning={true} />
-			<div className="flex w-full flex-col justify-around gap-16 px-10 pb-14 pt-44 sm:px-24 lg:px-56">
-				<h1 className="mb-4 w-full text-center font-press text-4xl leading-normal text-electric underline sm:text-5xl md:text-6xl">
-					MY SCRIPTS
+			<div className="flex w-full flex-col justify-around gap-16 px-10 pb-14 pt-44 sm:px-24 lg:px-80">
+				<h1 className="text-center font-vt text-4xl text-clay">
+					Create a new script to begin!
 				</h1>
 				<div className="flex gap-4 sm:gap-10">
 					<input
 						type="text"
 						name="title"
 						onChange={handleChange}
-						placeholder="script title..."
-						className="w-full rounded-lg px-4 py-2 outline-none"
+						placeholder="Script title..."
+						className="w-full rounded-3xl bg-slate-900 px-6 py-2 font-vt text-2xl text-clay outline-none"
 					/>
 					<button
-						className="w-40 rounded-lg bg-lime p-2 font-vt text-2xl text-dark hover:bg-electric sm:w-72"
+						className="w-40 rounded-3xl bg-lime p-2 font-vt text-2xl text-dark hover:bg-electric sm:w-72"
 						onClick={addScript}
 					>
-						Create Script
+						Create
 					</button>
 				</div>
 				<div className="flex flex-col gap-14">
