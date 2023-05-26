587b7c9b98273bdebbbb0b5f912d4ae7b2cf3c56
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-26 15:15:58 +0700

    feat: add delete and edit handling

--- src/components/item/ScriptItem.tsx
+++ src/components/item/ScriptItem.tsx
@@ -2,13 +2,26 @@ import { ScriptData } from "@/types/script-type";
 import { ScriptAction, ScriptActionType } from "@/types/scriptaction-type";
 import { Dispatch, FC } from "react";
 import { AiFillDelete } from "react-icons/ai";
-import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
 
 interface ScriptItemProps extends ScriptData {
 	dispatchScripts: Dispatch<ScriptAction>;
 }
 
 const ScriptItem: FC<ScriptItemProps> = (props: ScriptItemProps) => {
+	function handleDelete() {
+		props.dispatchScripts({
+			type: ScriptActionType.DELETE,
+			payload: { title: props.title },
+		});
+	}
+
+	function handleEdit() {
+		props.dispatchScripts({
+			type: ScriptActionType.EDIT,
+			payload: { title: props.title },
+		});
+	}
+
 	return (
 		<div key={props.title} className="rounded-lg bg-black bg-opacity-50">
 			{/* Top */}
@@ -18,8 +31,11 @@ const ScriptItem: FC<ScriptItemProps> = (props: ScriptItemProps) => {
 				</h1>
 
 				<div className="flex items-center gap-4">
-					<MdOutlineDriveFileRenameOutline className="h-6 w-6 cursor-pointer fill-lime hover:fill-clay" />
-					<AiFillDelete className="h-6 w-6 cursor-pointer fill-red-500 hover:fill-red-400" />
+					{/* <MdOutlineDriveFileRenameOutline className="h-6 w-6 cursor-pointer fill-lime hover:fill-clay" /> */}
+					<AiFillDelete
+						className="h-6 w-6 cursor-pointer fill-red-500 hover:fill-red-400"
+						onClick={handleDelete}
+					/>
 				</div>
 			</div>
 
@@ -27,7 +43,10 @@ const ScriptItem: FC<ScriptItemProps> = (props: ScriptItemProps) => {
 
 			{/* Bottom */}
 			<div className="relative h-36">
-				<button className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-lime px-6 py-2 font-vt text-xl hover:bg-electric hover:text-clay">
+				<button
+					className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-lime px-6 py-2 font-vt text-xl hover:bg-electric hover:text-clay"
+					onClick={handleEdit}
+				>
 					Edit
 				</button>
 			</div>
