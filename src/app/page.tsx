132bfbdad805b0d993f1b2a04d06982e3f306944
steven <stevenplayer25@gmail.com>
2023-05-27 23:35:55 +0700

    bug fix 1.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const WelcomePage: FC<WelcomePageProps> = (props: WelcomePageProps) => {
 				{/* Hero */}
 				<div className="paper-grid flex flex-col items-center justify-center gap-10 bg-dark px-8 py-28 md:px-16  lg:flex-row halfxl:flex-row halfxl:gap-0">
 					<div className="flex w-full flex-col items-center justify-center gap-10 px-0 sm:px-6 md:w-2/3 halfxl:w-full">
-						<p className="w-full text-center font-press text-lime sm:text-left">
+						<p className="w-full text-center font-press text-lime sm:text-left ml-20 md:ml-5">
 							LET&apos;S LEARN HOW TO
 						</p>
 						<div className="flex flex-col gap-8 font-press text-3xl sm:text-4xl halfxl:text-5xl">
