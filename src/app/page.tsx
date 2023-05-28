7c6274d61d9da38dc1a4c5fff7c076c608bebc83
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-28 13:59:47 +0700

    feat: change purpose text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const WelcomePage: FC<WelcomePageProps> = (props: WelcomePageProps) => {
 				{/* Hero */}
 				<div className="paper-grid flex flex-col items-center justify-center gap-10 bg-dark px-8 py-28 md:px-16  lg:flex-row halfxl:flex-row halfxl:gap-0">
 					<div className="flex w-full flex-col items-center justify-center gap-10 px-0 sm:px-6 md:w-2/3 halfxl:w-full">
-						<p className="w-full text-center font-press text-lime sm:text-left ml-20 md:ml-5">
+						<p className="ml-20 w-full text-center font-press text-lime sm:text-left md:ml-5">
 							LET&apos;S LEARN HOW TO
 						</p>
 						<div className="flex flex-col gap-8 font-press text-3xl sm:text-4xl halfxl:text-5xl">
@@ -44,26 +44,33 @@ const WelcomePage: FC<WelcomePageProps> = (props: WelcomePageProps) => {
 				</div>
 				{/* Purpose */}
 				<div className="paper-grid flex flex-col items-center justify-center gap-20 bg-dark px-8 py-14 md:px-16 halfxl:gap-0">
-					<h1 className="text-center font-press text-5xl leading-normal text-clay mb-20 md:mb-40">
+					<h1 className="mb-20 text-center font-press text-5xl leading-normal text-clay md:mb-40">
 						PURPOSE
 					</h1>
 					<div className="-mt-10 flex flex-col gap-2">
 						<div className="mb-20 flex flex-col items-center justify-center p-5 md:flex-row">
 							<Image
-								src="/profile.jpg"
+								src="/norbert_profile.png"
 								className="w-40 rounded-full border-2"
 								width={600}
 								height={600}
 								alt="profile"
 							/>
 							<div className="items-center justify-center md:ml-10">
-								<h2 className="my-5 text-center font-press text-3xl text-clay text-lime md:my-0 md:mb-5 md:text-left">
+								<h2 className="my-5 text-center font-press text-3xl text-lime md:my-0 md:mb-5 md:text-left">
 									Norbert Oliver
 								</h2>
 								<p className="text-center font-press text-xl text-clay md:pt-0 md:text-left">
-									Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Iste sapiente aut porro in
-									officiis ullam odit.
+									I started this project to pay homage to my
+									early days of learning how to program.
+									During highschool, I observed one of my
+									friends programming in batch script, which
+									ignited my passion for developing
+									captivating text-based adventure games. This
+									addiction drove me to delve deeper into
+									coding and expand my knowledge. I sincerely
+									hope that this website can assist you in
+									pursuing your programming journey.
 								</p>
 							</div>
 						</div>
